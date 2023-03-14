3f4deccb2daf0b5c51a37f9dec9ba42236facb53
AlexUngur1 <unguralexandru19@gmail.com>
2023-03-14 15:22:48 +0200

    test worked

--- main.tf
+++ main.tf
@@ -13,15 +13,15 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-alexungur1"
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+# resource "aws_s3_bucket" "b" {
+#   bucket = "my-tf-test-bucket-alexungur1"
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "example" {
+#   bucket = aws_s3_bucket.b.id
+#   acl    = "private"
+# }
