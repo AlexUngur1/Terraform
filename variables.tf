b79b0dcfde28eb8e25f7cf8ce6613b8a46df569e
AlexUngur1 <unguralexandru19@gmail.com>
2023-03-17 15:16:18 +0200

    up to date

--- variables.tf
+++ variables.tf
@@ -14,3 +14,20 @@
 #       # }
 #    # }
 # }
+variable "vm_count" {
+  description = "Number of VMs to create"
+  type        = number
+  default     = 2
+}
+
+variable "vm_flavor" {
+  description = "Flavor of the VM"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "vm_image" {
+  description = "AMI ID of the VM"
+  type        = string
+  default     = "ami-0c94855ba95c71c99"
+}
